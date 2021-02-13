70064e371e154ed2258a7870d9dfd140efa0f600
marcobonaccorso <marco.bonaccorso00@gmail.com>
2021-02-13 10:59:49 +0100

    primo commit
    creazione progetto jpa

---
+++ RubricaJPA/src/main/java/it/sirfin/RubricaJPA/RubricaJpaApplication.java
@@ -0,0 +1,13 @@
+package it.sirfin.RubricaJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubricaJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubricaJpaApplication.class, args);
+	}
+
+}
---
+++ RubricaJPA/src/test/java/it/sirfin/RubricaJPA/RubricaJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sirfin.RubricaJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubricaJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
